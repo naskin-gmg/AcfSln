6588fa59d20805b40b89916d67bd11046a49cdd3
wg <wg@3363cc13-2b78-0410-9281-ee1caeb4462d>
2009-11-13 00:03:34 +0000

    Script to copy DLL's for Release directory added.
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfSln@8 3363cc13-2b78-0410-9281-ee1caeb4462d

---
+++ Bin/CopyDll_Release.bat
@@ -0,0 +1,8 @@
+copy /Y %QTDIR%\lib\*.dll ReleaseVC8
+copy /Y %QTDIR%\lib\*.dll.Manifest ReleaseVC8
+xcopy /Y %QTDIR%\plugins\imageformats\*.* ReleaseVC8\imageformats\*.*
+xcopy /Y %QTDIR%\plugins\iconengines\*.* ReleaseVC8\iconengines\*.*
+
+copy /Y %AVTDIR%\FireGrab\*.dll ReleaseVC8
+copy /Y %MILDIR%\Dll\*.dll ReleaseVC8
+copy /Y %OPENCVDIR%\Bin\*.dll ReleaseVC8
