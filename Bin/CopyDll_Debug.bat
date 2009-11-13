db607d9fadb530e165602975bad4bbc5ffe28552
wg <wg@3363cc13-2b78-0410-9281-ee1caeb4462d>
2009-11-13 16:32:56 +0000

    Compositor extended to support external ACF.exe and allow to specify *.xpc files from GUI.
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfSln@12 3363cc13-2b78-0410-9281-ee1caeb4462d

--- Bin/CopyDll_Debug.bat
+++ Bin/CopyDll_Debug.bat
@@ -3,6 +3,8 @@ copy /Y %QTDIR%\lib\*.dll.Manifest DebugVC8
 xcopy /Y %QTDIR%\plugins\imageformats\*.* DebugVC8\imageformats\*.*
 xcopy /Y %QTDIR%\plugins\iconengines\*.* DebugVC8\iconengines\*.*
 
+copy /Y %ACFDIR%\Bin\DebugVC8\Acf.exe DebugVC8
+
 copy /Y %AVTDIR%\FireGrab\*.dll DebugVC8
 copy /Y %MILDIR%\Dll\*.dll DebugVC8
 copy /Y %OPENCVDIR%\Bin\*.dll DebugVC8
--- Bin/CopyDll_Release.bat
+++ Bin/CopyDll_Release.bat
@@ -3,6 +3,8 @@ copy /Y %QTDIR%\lib\*.dll.Manifest ReleaseVC8
 xcopy /Y %QTDIR%\plugins\imageformats\*.* ReleaseVC8\imageformats\*.*
 xcopy /Y %QTDIR%\plugins\iconengines\*.* ReleaseVC8\iconengines\*.*
 
+copy /Y %ACFDIR%\Bin\ReleaseVC8\Acf.exe ReleaseVC8
+
 copy /Y %AVTDIR%\FireGrab\*.dll ReleaseVC8
 copy /Y %MILDIR%\Dll\*.dll ReleaseVC8
 copy /Y %OPENCVDIR%\Bin\*.dll ReleaseVC8
