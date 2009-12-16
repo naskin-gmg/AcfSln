ff4c8f7583ab237c4cd8b9489078ecaa923eabce
kl <kl@3363cc13-2b78-0410-9281-ee1caeb4462d>
2009-12-16 13:58:03 +0000

    Evolution of hotfolder's item info GUI.
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfSln@99 3363cc13-2b78-0410-9281-ee1caeb4462d

--- Include/iqtfpf/CProcessingItemInfoGuiComp.cpp
+++ Include/iqtfpf/CProcessingItemInfoGuiComp.cpp
@@ -4,6 +4,8 @@
 // ACF includes
 #include "istd/TChangeNotifier.h"
 
+#include "iproc/IProcessor.h"
+
 #include "iqt/CSignalBlocker.h"
 
 
@@ -27,6 +29,26 @@ void CProcessingItemInfoGuiComp::UpdateEditor(int /*updateFlags*/)
 		InputFilePathLabel->setText(iqt::GetQString(objectPtr->GetInputFile()));
 		OutputFilePathLabel->setText(iqt::GetQString(objectPtr->GetOutputFile()));
 		ItemProgress->setValue(objectPtr->GetProgress() * 100);
+
+		int itemState = objectPtr->GetProcessingState();
+	
+		ItemProgress->setVisible(itemState == iproc::IProcessor::TS_WAIT || itemState == iproc::IProcessor::TS_NONE);
+		ProgressLabel->setVisible(itemState == iproc::IProcessor::TS_WAIT || itemState == iproc::IProcessor::TS_NONE);
+
+		switch (itemState){
+			case iproc::IProcessor::TS_OK:
+				StateLabel->setText(tr("Processing finished successfully"));
+				break;
+			case iproc::IProcessor::TS_INVALID:
+				StateLabel->setText(tr("Processing finished with any error(s)"));
+				break;
+			case iproc::IProcessor::TS_NONE:
+				StateLabel->setText(tr("Waiting for processing..."));
+				break;
+			case iproc::IProcessor::TS_CANCELED:
+				StateLabel->setText(tr("Processing was canceled"));
+				break;
+		}
 	}
 }
 
