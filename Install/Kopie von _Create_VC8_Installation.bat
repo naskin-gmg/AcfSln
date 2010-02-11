22794a09bbc8dd89fbc78964c4c97f61e1b468b3
wg <wg@3363cc13-2b78-0410-9281-ee1caeb4462d>
2010-02-11 10:36:58 +0000

    Tutorial registries moved to 'Partitura'.
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfSln@128 3363cc13-2b78-0410-9281-ee1caeb4462d

---
+++ Install/Kopie von _Create_VC8_Installation.bat
@@ -0,0 +1,35 @@
+echo off
+
+if not exist Temp goto DelTempSkip
+echo Removing old temporary files...
+rmdir /Q /S Temp
+:DelTempSkip
+
+if not exist TempExcl goto DelTempExclSkip
+echo Removing old temporary scripts...
+rmdir /Q /S TempExcl
+:DelTempExclSkip
+
+mkdir Temp
+mkdir TempExcl
+
+set COMPILER_EXT=VC8
+
+rem call Rebuild.bat
+
+rem call CopyDlls.bat
+
+rem call Create_TechnicalDoc.bat
+
+call Create_VC_LGPL.bat
+
+rem call Compile_Setup.bat
+
+echo Installation done
+
+pause
+
+echo Removing temporary files...
+rmdir /Q /S Temp
+rmdir /Q /S TempExcl
+
