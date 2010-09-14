9b4f71f9f75ef82526a99866472b42fe111708b8
wg <wg@3363cc13-2b78-0410-9281-ee1caeb4462d>
2010-09-14 03:39:58 +0000

    Cosmetic changes.
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfSln@241 3363cc13-2b78-0410-9281-ee1caeb4462d

--- Bin/CopyDll_DebugVC9.bat
+++ Bin/CopyDll_DebugVC9.bat
@@ -1,5 +1,4 @@
 copy /Y %QTDIR%\bin\*.dll DebugVC9
-copy /Y %QTDIR%\bin\*.dll.Manifest DebugVC9
 xcopy /Y %QTDIR%\plugins\imageformats\*.* DebugVC9\imageformats\*.*
 xcopy /Y %QTDIR%\plugins\iconengines\*.* DebugVC9\iconengines\*.*
 
--- Bin/CopyDll_ReleaseVC9.bat
+++ Bin/CopyDll_ReleaseVC9.bat
@@ -1,5 +1,4 @@
 copy /Y %QTDIR%\bin\*.dll ReleaseVC9
-copy /Y %QTDIR%\bin\*.dll.Manifest ReleaseVC9
 xcopy /Y %QTDIR%\plugins\imageformats\*.* ReleaseVC9\imageformats\*.*
 xcopy /Y %QTDIR%\plugins\iconengines\*.* ReleaseVC9\iconengines\*.*
 
