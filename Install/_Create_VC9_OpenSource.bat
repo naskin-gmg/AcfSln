e7e1e7da5652ebddaffd6f84a6bab682cd9f905b
wg <wg@3363cc13-2b78-0410-9281-ee1caeb4462d>
2010-10-19 09:59:13 +0000

    Scripts for installation of pure open source version added.
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfSln@275 3363cc13-2b78-0410-9281-ee1caeb4462d

---
+++ Install/_Create_VC9_OpenSource.bat
@@ -0,0 +1,22 @@
+echo off
+
+if not exist Temp goto DelTempSkip
+echo Removing old temporary files...
+rmdir /Q /S Temp
+:DelTempSkip
+
+if not exist TempExcl goto DelTempExclSkip
+echo Removing old temporary scripts...
+rmdir /Q /S TempExcl
+:DelTempExclSkip
+
+mkdir Temp
+mkdir TempExcl
+
+set COMPILER_EXT=VC9
+
+call Create_TechnicalDoc.bat
+
+echo Copying files to Temp directory...
+"%ACFDIR%/Bin/Debug%COMPILER_EXT%/Acf.exe" Create_VC_OpenSource.arx -config ../Config/Core.xpc
+
