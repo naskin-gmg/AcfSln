bf7e78458ee089bbdde26bcb541416ea814c683d
wg <wg@3363cc13-2b78-0410-9281-ee1caeb4462d>
2011-03-31 10:45:50 +0000

    Small corrections.
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfSln@316 3363cc13-2b78-0410-9281-ee1caeb4462d

--- Include/iqwt/CProgressHistoryGuiComp.cpp
+++ Include/iqwt/CProgressHistoryGuiComp.cpp
@@ -52,7 +52,7 @@ int CProgressHistoryGuiComp::BeginProgressSession(
 	sessionPtr->curve.attach(m_plotPtr.GetPtr());
 	sessionPtr->curve.setData(&m_axisXData[0], &m_axisXData[0], 0);
 
-	QColor lineColor = Qt::GlobalColor(Qt::cyan + Qt::red + int(qHash(progressId.c_str())) % (Qt::transparent - Qt::red));
+	QColor lineColor = Qt::GlobalColor(Qt::red + int(qHash(progressId.c_str())) % (Qt::transparent - Qt::red));
 
 	int colorsCount = istd::Min(m_progressIdsAttrPtr.GetCount(), m_progressColorsAttrPtr.GetCount());
 	for (int i = 0; i < colorsCount; ++i){
