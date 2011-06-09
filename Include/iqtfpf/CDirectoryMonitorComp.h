f30847e5ceb94932ec4270e23b0a667668e6de67
kl <kl@3363cc13-2b78-0410-9281-ee1caeb4462d>
2011-06-09 11:14:05 +0000

    Small bug fixed.
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfSln@344 3363cc13-2b78-0410-9281-ee1caeb4462d

--- Include/iqtfpf/CDirectoryMonitorComp.h
+++ Include/iqtfpf/CDirectoryMonitorComp.h
@@ -54,7 +54,7 @@ public:
 		I_ASSIGN_TO(m_paramsSetModelCompPtr, m_paramsSetCompPtr, false);
 		I_ASSIGN(m_monitoringSessionManagerCompPtr, "SessionManager", "Provider of previous monitoring sessions", false, "SessionManager");
 		I_ASSIGN(m_directoryPathIdAttrPtr, "DirectoryPathParamsId", "Parameter ID of the path to be observed in the parameter set", true, "DirectoryPathParamsId");
-		I_ASSIGN_TO(m_directoryMonitorParamsIdAttrPtr, m_directoryPathIdAttrPtr, true);
+		I_ASSIGN(m_directoryMonitorParamsIdAttrPtr, "DirectoryMonitorParamsId", "ID of the directory observing parameters in the parameter set", true, "DirectoryMonitorParamsId");
 		I_ASSIGN(m_autoStartAttrPtr, "AutoStart", "If enabled, start the directory monitoring after initialization", false, false);
 	I_END_COMPONENT;
 
