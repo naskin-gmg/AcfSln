e6bd9d8be8848ea137092bf9e43896191bcafe5a
wg <wg@3363cc13-2b78-0410-9281-ee1caeb4462d>
2011-09-07 12:34:00 +0000

    Issue #BUILD-1: Removing of garbages from technical documentation added.
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfSln@381 3363cc13-2b78-0410-9281-ee1caeb4462d

--- Install/Create_TechnicalDoc.bat
+++ Install/Create_TechnicalDoc.bat
@@ -1,5 +1,7 @@
 echo Creation of technical documentation...
 
+del /q /s ..\Docs\TechnicalDoc\*
+
 echo Create doxyfile...
 "%ACFDIR%/bin/Debug%COMPILER_EXT%/Acf.exe" Create_Doxyfile.arx -config ../Config/Core.xpc
 
