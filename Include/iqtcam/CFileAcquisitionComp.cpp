4bfa455568b168633f8101ac72d0ba458bdb312c
kl <kl@3363cc13-2b78-0410-9281-ee1caeb4462d>
2012-06-28 13:30:40 +0000

    Compiling issues fixed.
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfSln@583 3363cc13-2b78-0410-9281-ee1caeb4462d

--- Include/iqtcam/CFileAcquisitionComp.cpp
+++ Include/iqtcam/CFileAcquisitionComp.cpp
@@ -7,10 +7,10 @@
 #include <QtGui/QImageReader>
 #include <QtCore/QStringList>
 
+// ACF includes
 #include "istd/TChangeNotifier.h"
-
 #include "iprm/IParamsSet.h"
-
+#include "iimg/IRasterImage.h"
 #include "iqt/IQImageProvider.h"
 
 
