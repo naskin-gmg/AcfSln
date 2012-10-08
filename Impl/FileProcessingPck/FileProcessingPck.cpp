51ff40ff1540ff036a2c16d3d658f0c87afc98fa
kl <kl@3363cc13-2b78-0410-9281-ee1caeb4462d>
2012-10-08 08:17:10 +0000

    Missing files added.
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfSln@702 3363cc13-2b78-0410-9281-ee1caeb4462d

---
+++ Impl/FileProcessingPck/FileProcessingPck.cpp
@@ -0,0 +1,39 @@
+#include "FileProcessingPck.h"
+
+
+#include "icomp/export.h"
+
+
+namespace FileProcessingPck
+{
+
+
+I_EXPORT_PACKAGE(
+			"AcfSln/FileProcessing",
+			"File Processing package",
+			"File" IM_TAG("Processing") IM_PROJECT("ACF-Solutions") IM_COMPANY("ImagingTools") IM_AUTHOR("\"Witold Gantzke\" \"Kirill Lepskiy\""));
+
+I_EXPORT_COMPONENT(
+			FileNaming,
+			"Component for file (re)naming",
+			"File Renaming Directory");
+
+I_EXPORT_COMPONENT(
+			FileNamingParams,
+			"Parmeter for file (re)naming",
+			"File Processing Hotfolder Parameter");
+
+I_EXPORT_COMPONENT(
+			FileListConverter,
+			"Component for conversion of a list of files",
+			"File Processing Conversion List");
+
+I_EXPORT_COMPONENT(
+			FilesContainer,
+			"A simple container of file pathes",
+			"File List Container");
+
+
+} // namespace FileProcessingPck
+
+
---
+++ Impl/FileProcessingPck/FileProcessingPck.h
@@ -0,0 +1,36 @@
+#ifndef FileProcessingPck_included
+#define FileProcessingPck_included
+
+
+#include "icomp/TModelCompWrap.h"
+#include "icomp/TMakeComponentWrap.h"
+
+#include "ifileproc/CFileNamingComp.h"
+#include "ifileproc/CFileNamingParamsComp.h"
+#include "ifileproc/CFileListConverterComp.h"
+#include "ifileproc/CFilePathesContainer.h"
+
+
+/**
+	System-undependent package for File Processing Framework.
+*/
+namespace FileProcessingPck
+{
+
+
+typedef ifileproc::CFileNamingComp FileNaming;
+
+typedef icomp::TModelCompWrap<ifileproc::CFileNamingParamsComp> FileNamingParams;
+
+typedef ifileproc::CFileListConverterComp FileListConverter;
+
+typedef icomp::TMakeComponentWrap<
+			ifileproc::CFilePathesContainer, ibase::IFileListProvider, istd::IChangeable> FilesContainer;
+
+
+} // namespace FileProcessingPck
+
+
+#endif // !FileProcessingPck_included
+
+
