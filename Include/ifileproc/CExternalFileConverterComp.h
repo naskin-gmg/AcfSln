4472072cf5fc32b9944219a27866fa610e10865e
kl <kl@3363cc13-2b78-0410-9281-ee1caeb4462d>
2012-10-13 22:36:03 +0000

    Detailed description for CExternalFileConverterComp added.
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfSln@719 3363cc13-2b78-0410-9281-ee1caeb4462d

--- Include/ifileproc/CExternalFileConverterComp.h
+++ Include/ifileproc/CExternalFileConverterComp.h
@@ -18,7 +18,20 @@ namespace ifileproc
 
 
 /**
-	File converter, which uses an external programm to perform the convert action. 
+	File converter, which uses an external programm to perform the convert action.
+	There are several ways to define how the command line parameter can be passed to the underlaying executable process.
+	The simplest way is to define the complete command line in text attribute \c DefaultProcessArguments.
+	It works only for use cases, where the command line can be statically defined and is known at the component definition time.
+	For the situation, where the process arguments can be changed at runtime,
+	you can use \c m_additionalArgumentsCompPtr. This is a list of iprm::INameParam objects.
+	For integration into the command line, you can use placeholder (variable) in the form $(An),
+	where c\ n is the index in the object list of \c additionalArgumentsCompPtr.
+	For example, in the command line <MyProcess.exe $(A1)>,
+	the $(A1) argument will be replaced by the additionalArgumentsCompPtr[0]->GetName().
+	For defining of the command line you can also use some predefined variables. Currently, following variables are supported:
+	$(Input) - \c inputPath argument of ConvertFiles method is used.
+	$(Output) - \c outputPath argument of ConvertFiles method is used.
+	$(OutputDir) - Output directory extracted from \c outputPath argument of ConvertFiles method is used.
 */
 class CExternalFileConverterComp:
 			public QObject,
