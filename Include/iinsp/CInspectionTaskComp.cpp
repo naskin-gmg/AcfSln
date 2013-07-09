e3541f2bde5327a77906423375925430c17e4e1f
am <am@3363cc13-2b78-0410-9281-ee1caeb4462d>
2013-07-09 12:54:17 +0000

    Changes from Alex.
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfSln@960 3363cc13-2b78-0410-9281-ee1caeb4462d

--- Include/iinsp/CInspectionTaskComp.cpp
+++ Include/iinsp/CInspectionTaskComp.cpp
@@ -1,3 +1,25 @@
+/********************************************************************************
+**
+**	Copyright (c) 2007-2011 Witold Gantzke & Kirill Lepskiy
+**
+**	This file is part of the ACF-Solutions Toolkit.
+**
+**	This file may be used under the terms of the GNU Lesser
+**	General Public License version 2.1 as published by the Free Software
+**	Foundation and appearing in the file LicenseLGPL.txt included in the
+**	packaging of this file.  Please review the following information to
+**	ensure the GNU Lesser General Public License version 2.1 requirements
+**	will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+**	If you are unsure which license is appropriate for your use, please
+**	contact us at info@imagingtools.de.
+**
+** 	See http://www.ilena.org, write info@imagingtools.de or contact
+**	by Skype to ACF_infoline for further information about the ACF-Solutions.
+**
+********************************************************************************/
+
+
 #include "iinsp/CInspectionTaskComp.h"
 
 
@@ -309,6 +331,7 @@ void CInspectionTaskComp::EnsureStatusKnown()
 				int workStatus = supplierPtr->GetWorkStatus();
 
 				if (workStatus < WS_OK){	// no result was calculated for this subtask
+					m_resultCategory = IC_NONE;
 					continue;
 				}
 
--- Include/iipr/CImageCropProcessorComp.h
+++ Include/iipr/CImageCropProcessorComp.h
@@ -1,3 +1,25 @@
+/********************************************************************************
+**
+**	Copyright (c) 2007-2011 Witold Gantzke & Kirill Lepskiy
+**
+**	This file is part of the ACF-Solutions Toolkit.
+**
+**	This file may be used under the terms of the GNU Lesser
+**	General Public License version 2.1 as published by the Free Software
+**	Foundation and appearing in the file LicenseLGPL.txt included in the
+**	packaging of this file.  Please review the following information to
+**	ensure the GNU Lesser General Public License version 2.1 requirements
+**	will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+**	If you are unsure which license is appropriate for your use, please
+**	contact us at info@imagingtools.de.
+**
+** 	See http://www.ilena.org, write info@imagingtools.de or contact
+**	by Skype to ACF_infoline for further information about the ACF-Solutions.
+**
+********************************************************************************/
+
+
 #ifndef iipr_CImageCropProcessorComp_included
 #define iipr_CImageCropProcessorComp_included
 
@@ -22,7 +44,6 @@ public:
 	I_BEGIN_COMPONENT(CImageCropProcessorComp);
 	I_END_COMPONENT;
 
-protected:
 	// reimplemented (CImageRegionProcessorCompBase)
 	virtual bool ProcessImageRegion(
 				const iimg::IBitmap& inputBitmap,
