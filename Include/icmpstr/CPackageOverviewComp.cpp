dd61673e9ec599a5c4422307fefba00548caa7a8
wg <wg@3363cc13-2b78-0410-9281-ee1caeb4462d>
2014-08-27 21:38:33 +0000

    Compositor: Problem with no meta information of embedded component was partially fixed.
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfSln@1190 3363cc13-2b78-0410-9281-ee1caeb4462d

--- Include/icmpstr/CPackageOverviewComp.cpp
+++ Include/icmpstr/CPackageOverviewComp.cpp
@@ -1016,11 +1016,15 @@ void CPackageOverviewComp::UpdateGui(const istd::IChangeable::ChangeSet& changeS
 		}
 
 		if (!changeSet.Contains(IElementSelectionInfo::CF_SELECTION)){	// ignore selection only changes
+			m_localMetaInfoManager.UpdateLocalMetaInfoMap();
+
 			GenerateComponentTree(false);
 		}
 	}
 	else{
 		m_registryObserver.EnsureModelDetached();
+
+		m_localMetaInfoManager.UpdateLocalMetaInfoMap();
 
 		GenerateComponentTree(false);
 	}
