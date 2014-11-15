d49a516b67b5e9f68acbbc6b20f1dbb4b076c20c
kl <kl@3363cc13-2b78-0410-9281-ee1caeb4462d>
2014-11-15 18:35:44 +0000

    Scripts corrected
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfSln@1211 3363cc13-2b78-0410-9281-ee1caeb4462d

--- Build/VC9/CopyDllToBin_Debug.bat
+++ Build/VC9/CopyDllToBin_Debug.bat
@@ -1,4 +1,4 @@
-copy /Y %QTDIR%\bin\*.dll ..\..\Bin\DebugVC9
+copy /Y %QTDIR%\bin.debug\*.dll ..\..\Bin\DebugVC9
 xcopy /Y %QTDIR%\plugins\imageformats\*.* ..\..\Bin\DebugVC9\imageformats\*.*
 xcopy /Y %QTDIR%\plugins\iconengines\*.* ..\..\Bin\DebugVC9\iconengines\*.*
 
