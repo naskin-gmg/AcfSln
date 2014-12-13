87d76a9c512ff8a43e3b0b868271b6a99a2744ed
kl <kl@3363cc13-2b78-0410-9281-ee1caeb4462d>
2014-12-13 12:37:29 +0000

    General supplier's GUI implementation made more flexible.
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfSln@1213 3363cc13-2b78-0410-9281-ee1caeb4462d

--- Include/iqtinsp/CGeneralSupplierGuiComp.cpp
+++ Include/iqtinsp/CGeneralSupplierGuiComp.cpp
@@ -1,11 +1,5 @@
 #include "iqtinsp/CGeneralSupplierGuiComp.h"
 
-// Qt includes
-#include <QtCore/QTimer>
-
-// ACF includes
-#include "istd/CChangeNotifier.h"
-
 
 namespace iqtinsp
 {
@@ -20,23 +14,7 @@ CGeneralSupplierGuiComp::CGeneralSupplierGuiComp()
 
 void CGeneralSupplierGuiComp::on_TestButton_clicked()
 {
-	iinsp::ISupplier* supplierPtr = GetObjectPtr();
-	if (supplierPtr != NULL){
-		supplierPtr->InvalidateSupplier();
-		supplierPtr->EnsureWorkInitialized();
-		supplierPtr->EnsureWorkFinished();
-
-		if (supplierPtr->GetWorkStatus() >= iinsp::ISupplier::WS_ERROR){
-			QMessageBox::warning(
-						GetQtWidget(),
-						QObject::tr("Error"),
-						QObject::tr("Processing Error"));
-
-			if (AutoTestButton->isChecked()){
-				AutoTestButton->setChecked(false);
-			}
-		}
-	}
+	Test();
 }
 
 
@@ -51,128 +29,6 @@ void CGeneralSupplierGuiComp::on_SaveParamsButton_clicked()
 	SaveParams();
 }
 
-
-// protected methods
-
-// reimplemented (iqtinsp::TSupplierGuiCompBase)
-
-QWidget* CGeneralSupplierGuiComp::GetParamsWidget() const
-{
-	Q_ASSERT(IsGuiCreated());
-
-	return ParamsFrame;
-}
-
-
-void CGeneralSupplierGuiComp::OnSupplierParamsChanged()
-{
-	if (IsGuiCreated() && AutoTestButton->isChecked()){
-		QTimer::singleShot(1, this, SLOT(on_TestButton_clicked()));
-	}
-}
-
-
-// reimplemented (iqtgui::TGuiObserverWrap)
-
-void CGeneralSupplierGuiComp::OnGuiModelAttached()
-{
-	BaseClass::OnGuiModelAttached();
-
-	ParamsGB->setVisible(AreParamsEditable() || IsLoadParamsSupported());
-
-	LoadParamsButton->setVisible(IsLoadParamsSupported());
-	SaveParamsButton->setVisible(IsSaveParamsSupported());
-}
-
-
-void CGeneralSupplierGuiComp::OnGuiHidden()
-{
-	AutoTestButton->setChecked(false);
-
-	BaseClass::OnGuiHidden();
-}
-
-
-void CGeneralSupplierGuiComp::UpdateGui(const istd::IChangeable::ChangeSet& changeSet)
-{
-	BaseClass::UpdateGui(changeSet);
-
-	Q_ASSERT(IsGuiCreated());
-
-	QString statusLabelText = tr("Unknown");
-
-	QString description;
-
-	const iinsp::ISupplier* supplierPtr = GetObjectPtr();
-	if (supplierPtr != NULL){
-		const istd::IInformationProvider* infoProviderPtr = dynamic_cast<const istd::IInformationProvider*>(supplierPtr);
-
-		int workStatus = supplierPtr->GetWorkStatus();
-
-		switch (workStatus){
-		case iinsp::ISupplier::WS_INVALID:
-			statusLabelText = tr("None");
-			break;
-
-		case iinsp::ISupplier::WS_INIT:
-			statusLabelText = tr("Init");
-			break;
-
-		case iinsp::ISupplier::WS_LOCKED:
-			statusLabelText = tr("Locked");
-			break;
-
-		case iinsp::ISupplier::WS_OK:
-			if (infoProviderPtr != NULL){
-				switch (infoProviderPtr->GetInformationCategory()){
-				case istd::IInformationProvider::IC_WARNING:
-					statusLabelText = tr("Warning");
-					break;
-
-				case istd::IInformationProvider::IC_ERROR:
-					statusLabelText = tr("Error");
-					break;
-
-				default:
-					statusLabelText = tr("OK");
-					break;
-				}
-			}
-			else{
-				statusLabelText = tr("OK");
-			}
-
-			break;
-
-		case iinsp::ISupplier::WS_CANCELED:
-			statusLabelText = tr("Canceled");
-			break;
-
-		case iinsp::ISupplier::WS_ERROR:
-			statusLabelText = tr("Not processed");
-			break;
-
-		case iinsp::ISupplier::WS_CRITICAL:
-			statusLabelText = tr("Critical");
-			break;
-		}
-
-		if (infoProviderPtr != NULL){
-			description = infoProviderPtr->GetInformationDescription();
-		}
-
-		UpdateAllViews();
-	}
-
-	StatusLabel->setText(statusLabelText);
-	if (!description.isEmpty()){
-		DescriptionLabel->setText(description);
-		DescriptionLabel->setVisible(true);
-	}
-	else{
-		DescriptionLabel->setVisible(false);
-	}
-}
 
 
 } // namespace iqtinsp
--- Include/iqtinsp/CGeneralSupplierGuiComp.h
+++ Include/iqtinsp/CGeneralSupplierGuiComp.h
@@ -2,7 +2,11 @@
 #define iqtinsp_CGeneralSupplierGuiComp_included
 
 
+// Qt includes
+#include <QtCore/QTimer>
+
 // ACF includes
+#include "istd/CChangeNotifier.h"
 #include "iinsp/ISupplier.h"
 
 // ACF-Solutions includes
@@ -15,23 +19,15 @@ namespace iqtinsp
 {
 
 
-class CGeneralSupplierGuiComp:
-			public TSupplierGuiCompBase<Ui::CGeneralSupplierGuiComp, iinsp::ISupplier>
+template <class UI = Ui::CGeneralSupplierGuiComp>
+class TGeneralSupplierGuiComp: public TSupplierGuiCompBase<UI, iinsp::ISupplier>
 {
-	Q_OBJECT
-
 public:
-	typedef TSupplierGuiCompBase<Ui::CGeneralSupplierGuiComp, iinsp::ISupplier> BaseClass;
+	typedef TSupplierGuiCompBase<UI, iinsp::ISupplier> BaseClass;
 
-	I_BEGIN_COMPONENT(CGeneralSupplierGuiComp);
-	I_END_COMPONENT;
-
-	CGeneralSupplierGuiComp();
-
-protected Q_SLOTS:
-	void on_TestButton_clicked();
-	void on_LoadParamsButton_clicked();
-	void on_SaveParamsButton_clicked();
+protected:
+	// pseudo slots
+	virtual void Test();
 
 protected:
 	// reimplemented (iqtinsp::TSupplierGuiCompBase)
@@ -42,6 +38,180 @@ protected:
 	virtual void OnGuiModelAttached();
 	virtual void OnGuiHidden();
 	virtual void UpdateGui(const istd::IChangeable::ChangeSet& changeSet);
+};
+
+
+
+// protected methods
+
+template <class UI>
+void TGeneralSupplierGuiComp<UI>::Test()
+{
+	iinsp::ISupplier* supplierPtr = GetObjectPtr();
+	if (supplierPtr != NULL){
+		supplierPtr->InvalidateSupplier();
+		supplierPtr->EnsureWorkInitialized();
+		supplierPtr->EnsureWorkFinished();
+
+		if (supplierPtr->GetWorkStatus() >= iinsp::ISupplier::WS_ERROR){
+			QMessageBox::warning(
+				GetQtWidget(),
+				QObject::tr("Error"),
+				QObject::tr("Processing Error"));
+
+			if (AutoTestButton->isChecked()){
+				AutoTestButton->setChecked(false);
+			}
+		}
+	}
+}
+
+
+// reimplemented (iqtinsp::TSupplierGuiCompBase)
+
+template <class UI>
+QWidget* TGeneralSupplierGuiComp<UI>::GetParamsWidget() const
+{
+	Q_ASSERT(IsGuiCreated());
+
+	return ParamsFrame;
+}
+
+
+template <class UI>
+void TGeneralSupplierGuiComp<UI>::OnSupplierParamsChanged()
+{
+	if (IsGuiCreated() && AutoTestButton->isChecked()){
+		QTimer::singleShot(1, this, SLOT(on_TestButton_clicked()));
+	}
+}
+
+
+// reimplemented (iqtgui::TGuiObserverWrap)
+
+template <class UI>
+void TGeneralSupplierGuiComp<UI>::OnGuiModelAttached()
+{
+	BaseClass::OnGuiModelAttached();
+
+	ParamsGB->setVisible(AreParamsEditable() || IsLoadParamsSupported());
+
+	LoadParamsButton->setVisible(IsLoadParamsSupported());
+	SaveParamsButton->setVisible(IsSaveParamsSupported());
+}
+
+
+template <class UI>
+void TGeneralSupplierGuiComp<UI>::OnGuiHidden()
+{
+	AutoTestButton->setChecked(false);
+
+	BaseClass::OnGuiHidden();
+}
+
+
+template <class UI>
+void TGeneralSupplierGuiComp<UI>::UpdateGui(const istd::IChangeable::ChangeSet& changeSet)
+{
+	BaseClass::UpdateGui(changeSet);
+
+	Q_ASSERT(IsGuiCreated());
+
+	QString statusLabelText = tr("Unknown");
+
+	QString description;
+
+	const iinsp::ISupplier* supplierPtr = GetObjectPtr();
+	if (supplierPtr != NULL){
+		const istd::IInformationProvider* infoProviderPtr = dynamic_cast<const istd::IInformationProvider*>(supplierPtr);
+
+		int workStatus = supplierPtr->GetWorkStatus();
+
+		switch (workStatus){
+			case iinsp::ISupplier::WS_INVALID:
+				statusLabelText = tr("None");
+				break;
+
+			case iinsp::ISupplier::WS_INIT:
+				statusLabelText = tr("Init");
+				break;
+
+			case iinsp::ISupplier::WS_LOCKED:
+				statusLabelText = tr("Locked");
+				break;
+
+			case iinsp::ISupplier::WS_OK:
+				if (infoProviderPtr != NULL){
+					switch (infoProviderPtr->GetInformationCategory()){
+			case istd::IInformationProvider::IC_WARNING:
+				statusLabelText = tr("Warning");
+				break;
+
+			case istd::IInformationProvider::IC_ERROR:
+				statusLabelText = tr("Error");
+				break;
+
+			default:
+				statusLabelText = tr("OK");
+				break;
+		}
+	}
+	else{
+		statusLabelText = tr("OK");
+	}
+
+	break;
+
+		case iinsp::ISupplier::WS_CANCELED:
+			statusLabelText = tr("Canceled");
+			break;
+
+		case iinsp::ISupplier::WS_ERROR:
+			statusLabelText = tr("Not processed");
+			break;
+
+		case iinsp::ISupplier::WS_CRITICAL:
+			statusLabelText = tr("Critical");
+			break;
+		}
+
+		if (infoProviderPtr != NULL){
+			description = infoProviderPtr->GetInformationDescription();
+		}
+
+		UpdateAllViews();
+	}
+
+	StatusLabel->setText(statusLabelText);
+	if (!description.isEmpty()){
+		DescriptionLabel->setText(description);
+		DescriptionLabel->setVisible(true);
+	}
+	else{
+		DescriptionLabel->setVisible(false);
+	}
+}
+
+
+/**
+	Default implementation of the supplier's UI
+*/
+class CGeneralSupplierGuiComp: public TGeneralSupplierGuiComp<>
+{
+	Q_OBJECT
+
+public:
+	typedef TGeneralSupplierGuiComp<> BaseClass;
+
+	I_BEGIN_COMPONENT(CGeneralSupplierGuiComp);
+	I_END_COMPONENT;
+
+	CGeneralSupplierGuiComp();
+
+protected Q_SLOTS:
+	void on_TestButton_clicked();
+	void on_LoadParamsButton_clicked();
+	void on_SaveParamsButton_clicked();
 };
 
 
