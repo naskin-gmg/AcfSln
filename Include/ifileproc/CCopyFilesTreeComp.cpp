685f534ac4e9af6f818083c7080cf8a3a959fbd0
kl <kl@3363cc13-2b78-0410-9281-ee1caeb4462d>
2015-08-03 13:34:58 +0000

    Build fixed. Garbages removed.
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfSln@1328 3363cc13-2b78-0410-9281-ee1caeb4462d

--- Include/ifileproc/CCopyFilesTreeComp.cpp
+++ Include/ifileproc/CCopyFilesTreeComp.cpp
@@ -1,31 +1,8 @@
-/********************************************************************************
-**
-**	Copyright (c) 2007-2014 Witold Gantzke & Kirill Lepskiy
-**
-**	This file is part of the ACF-Solutions Toolkit.
-**
-**	This file may be used under the terms of the GNU Lesser
-**	General Public License version 2.1 as published by the Free Software
-**	Foundation and appearing in the file LicenseLGPL.txt included in the
-**	packaging of this file.  Please review the following information to
-**	ensure the GNU Lesser General Public License version 2.1 requirements
-**	will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
-**
-**	If you are unsure which license is appropriate for your use, please
-**	contact us at info@imagingtools.de.
-**
-** 	See http://www.ilena.org, write info@imagingtools.de or contact
-**	by Skype to ACF_infoline for further information about the ACF-Solutions.
-**
-********************************************************************************/
-
-
 #include "ifileproc/CCopyFilesTreeComp.h"
 
 
 // Qt includes
 #include <QtCore/QRegExp>
-
 
 // ACF includes
 #include "istd/CSystem.h"
--- Include/ifileproc/CFileConvertOverProcessorComp.h
+++ Include/ifileproc/CFileConvertOverProcessorComp.h
@@ -1,25 +1,3 @@
-/********************************************************************************
-**
-**	Copyright (c) 2007-2014 Witold Gantzke & Kirill Lepskiy
-**
-**	This file is part of the ACF-Solutions Toolkit.
-**
-**	This file may be used under the terms of the GNU Lesser
-**	General Public License version 2.1 as published by the Free Software
-**	Foundation and appearing in the file LicenseLGPL.txt included in the
-**	packaging of this file.  Please review the following information to
-**	ensure the GNU Lesser General Public License version 2.1 requirements
-**	will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
-**
-**	If you are unsure which license is appropriate for your use, please
-**	contact us at info@imagingtools.de.
-**
-** 	See http://www.ilena.org, write info@imagingtools.de or contact
-**	by Skype to ACF_infoline for further information about the ACF-Solutions.
-**
-********************************************************************************/
-
-
 #ifndef ifileproc_CFileConvertOverProcessorComp_included
 #define ifileproc_CFileConvertOverProcessorComp_included
 
@@ -29,14 +7,13 @@
 
 // ACF includes
 #include "istd/TSmartPtr.h"
-
 #include "ifile/IFilePersistence.h"
-
-#include "ifileproc/IFileConversion.h"
 #include "ilog/TLoggerCompWrap.h"
-
 #include "ibase/IProgressManager.h"
+
+// ACF-Solutions includes
 #include "iproc/IProcessor.h"
+#include "ifileproc/IFileConversion.h"
 
 
 namespace ifileproc
