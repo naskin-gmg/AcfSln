fe5e946e0d0f773f1553fd0aa2a642959170f84a
wg <wg@3363cc13-2b78-0410-9281-ee1caeb4462d>
2015-09-22 10:01:32 +0000

    Installation scripts corrected.
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfSln@1403 3363cc13-2b78-0410-9281-ee1caeb4462d

--- Install/_Create_VC9_Installation.bat
+++ Install/_Create_VC9_Installation.bat
@@ -4,27 +4,24 @@ set COMPILER_EXT=VC9
 set LICENSE_TYPE=LGPL
 set LICENSE_DIR=Install/LGPL
 set LICENSE_INSTALL_PATH=../LGPL/License.txt
-set ACF_CONFIG_FILE=$(ACFSLNDIR)/Config/Core.xpc
+set ACF_CONFIG_FILE=%~dp0/../Config/Core.xpc
 
-cd ..
+cd %~dp0/..
 
-call Install\InternalParts\CreateTempDirs.bat
+call %ACFDIR%\Install\InternalParts\CreateTempDirs.bat
 
-call Install\InternalParts\Rebuild.bat
+call %ACFDIR%\Install\InternalParts\Rebuild.bat
 
-call Install\InternalParts\CopyDlls.bat
+call %ACFDIR%\Install\InternalParts\CopyDlls.bat
 
-call Install\InternalParts\CreateScripts.bat
+call %ACFDIR%\Install\InternalParts\CreateScripts.bat
 
-call Install\InternalParts\Create_TechnicalDoc.bat
+call %ACFDIR%\Install\InternalParts\Create_TechnicalDoc.bat
 
-call Install\InternalParts\CopyTempFiles.bat
+call %ACFDIR%\Install\InternalParts\CopyTempFiles.bat
 
-call Install\InternalParts\Compile_Setup.bat
+call %ACFDIR%\Install\InternalParts\Compile_Setup.bat
 
 echo Installation done
 
-pause
-
-call Install\InternalParts\RemoveTempDirs.bat
-
+call %ACFDIR%\Install\InternalParts\RemoveTempDirs.bat
--- Install/_Create_VC9_OpenSource.bat
+++ Install/_Create_VC9_OpenSource.bat
@@ -4,9 +4,12 @@ set COMPILER_EXT=VC9
 set LICENSE_TYPE=OpenSource
 set LICENSE_DIR=Install/LGPL
 set LICENSE_INSTALL_PATH=../LGPL/License.txt
-set ACF_CONFIG_FILE=$(ACFSLNDIR)/Config/Core.xpc
+set ACF_CONFIG_FILE=%~dp0/../Config/Core.xpc
 
-cd ..
+call %~dp0\..\Build\VC10\_CreateVCProjectsFromQMake.bat
+call %~dp0\..\Build\VC12\_CreateVCProjectsFromQMake.bat
+
+cd %~dp0\..
 
 call %ACFDIR%\Install\InternalParts\CreateTempDirs.bat
 
@@ -15,6 +18,3 @@ call %ACFDIR%\Install\InternalParts\CreateScripts.bat
 call %ACFDIR%\Install\InternalParts\Create_TechnicalDoc.bat
 
 call %ACFDIR%\Install\InternalParts\CopyTempFiles.bat
-
-pause
-
