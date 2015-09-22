e073319060a17149c1d3be827943f49daaa4f4ef
wg <wg@3363cc13-2b78-0410-9281-ee1caeb4462d>
2015-09-22 16:02:16 +0000

    VC12 installations added.
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfSln@1407 3363cc13-2b78-0410-9281-ee1caeb4462d

---
+++ Install/_Create_VC12_Installation.bat
@@ -0,0 +1,31 @@
+echo off
+
+set COMPILER_EXT=VC12
+set LICENSE_TYPE=LGPL
+set LICENSE_DIR=Install/LGPL
+set LICENSE_INSTALL_PATH=../LGPL/License.txt
+set ACF_CONFIG_FILE=%~dp0/../Config/Core.xpc
+
+call %~dp0\..\Build\VC12\_CreateVCProjectsFromQMake.bat
+
+cd %~dp0/..
+
+call %ACFDIR%\Install\InternalParts\CreateTempDirs.bat
+
+echo Compiling...
+vcbuild /r %~dp0\..\Build\%COMPILER_EXT%\AcfSlnAll.sln $All
+echo Compiling done
+
+call %ACFDIR%\Install\InternalParts\CopyDlls.bat
+
+call %ACFDIR%\Install\InternalParts\CreateScripts.bat
+
+call %ACFDIR%\Install\InternalParts\Create_TechnicalDoc.bat
+
+call %ACFDIR%\Install\InternalParts\CopyTempFiles.bat
+
+call %ACFDIR%\Install\InternalParts\Compile_Setup.bat
+
+echo Installation done
+
+call %ACFDIR%\Install\InternalParts\RemoveTempDirs.bat
---
+++ Install/_Create_VC12_OpenSource.bat
@@ -0,0 +1,17 @@
+echo off
+
+set COMPILER_EXT=VC12
+set LICENSE_TYPE=OpenSource
+set LICENSE_DIR=Install/LGPL
+set LICENSE_INSTALL_PATH=../LGPL/License.txt
+set ACF_CONFIG_FILE=$(ACFSLNDIR)/Config/Core.xpc
+
+cd ..
+
+call %ACFDIR%\Install\InternalParts\CreateTempDirs.bat
+
+call %ACFDIR%\Install\InternalParts\CreateScripts.bat
+
+call %ACFDIR%\Install\InternalParts\Create_TechnicalDoc.bat
+
+call %ACFDIR%\Install\InternalParts\CopyTempFiles.bat
