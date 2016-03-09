97043d7e24fd0ace19a880b963e708d146e5586a
wg <wg@3363cc13-2b78-0410-9281-ee1caeb4462d>
2016-03-09 11:09:56 +0000

    Scripts corrected.
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfSln@1551 3363cc13-2b78-0410-9281-ee1caeb4462d

--- Install/_Create_VC12_Public.bat
+++ Install/_Create_VC12_Public.bat
@@ -5,14 +5,13 @@ set LICENSE_DIR=Install/LGPL
 set LICENSE_INSTALL_PATH=../LGPL/License.txt
 set ACF_CONFIG_FILE=%~dp0/../Config/AcfSlnConfig.awc
 
-call %~dp0\..\Build\VC12\_CreateVCProjectsFromQMake.bat
-
-cd %~dp0\..
-
-set COMPILER_EXT=VC12
 call %ACFDIR%\Install\InternalParts\CreateTempDirs.bat
 
-call Install\InternalParts\CopyDlls.bat
+set COMPILER_EXT=VC12
+
+call %~dp0\..\Build\%COMPILER_EXT%\_CreateVCProjectsFromQMake.bat
+
+cd %~dp0\..
 
 echo Compiling...
 msbuild %~dp0\..\Build\%COMPILER_EXT%\AcfSlnAll.sln /p:Configuration=Debug
