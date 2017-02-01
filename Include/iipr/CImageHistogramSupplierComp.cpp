9f1bf521e0af6cf79400d0286df388cc660d8137
wg <wg@3363cc13-2b78-0410-9281-ee1caeb4462d>
2017-02-01 16:08:31 +0000

    Some initialization trash removed.
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfSln@1771 3363cc13-2b78-0410-9281-ee1caeb4462d

--- Include/iipr/CImageHistogramSupplierComp.cpp
+++ Include/iipr/CImageHistogramSupplierComp.cpp
@@ -11,8 +11,6 @@ namespace iipr
 
 const imeas::IDataSequence* CImageHistogramSupplierComp::GetDataSequence() const
 {
-	(const_cast<CImageHistogramSupplierComp*>(this))->EnsureWorkInitialized();
-
 	return GetWorkProduct();
 }
 
