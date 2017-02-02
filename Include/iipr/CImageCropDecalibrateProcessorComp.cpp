032bf708a09c6e7bd4b8b337f672a7479b691e15
wg <wg@3363cc13-2b78-0410-9281-ee1caeb4462d>
2017-02-02 15:44:16 +0000

    Crop processor corrected to process calibration and behave original pixel size.
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfSln@1773 3363cc13-2b78-0410-9281-ee1caeb4462d

--- Include/iipr/CImageCropDecalibrateProcessorComp.cpp
+++ Include/iipr/CImageCropDecalibrateProcessorComp.cpp
@@ -13,31 +13,38 @@ namespace iipr
 
 
 bool CImageCropDecalibrateProcessorComp::CropImage(
-			const iimg::IBitmap& inputBitmap,
 			const i2d::CRectangle& sourceAoi,
+			int cellSize,
+			const iimg::IBitmap& inputBitmap,
 			iimg::IBitmap& outputBitmap)
 {
-	istd::CIndex2d imageSize = inputBitmap.GetImageSize();
-	if (!outputBitmap.CreateBitmap(iimg::IBitmap::PF_GRAY, imageSize)){
+	if (cellSize < 2){
 		return false;
 	}
 
-	const i2d::ICalibration2d* calibrationPtr = sourceAoi.GetCalibration();
+	istd::CIndex2d outputImageSize;
+	if (!CalcOutputImageSize(sourceAoi, outputImageSize)){
+		return false;
+	}
 
-	const int cellSize = 8;
+	istd::CIndex2d gridSize((outputImageSize.GetX() + cellSize - 1) / cellSize + 1, (outputImageSize.GetY() + cellSize - 1) / cellSize + 1);
+	if (!outputBitmap.CreateBitmap(iimg::IBitmap::PF_GRAY, outputImageSize)){
+		return false;
+	}
 
-	istd::CIndex2d gridSize(imageSize.GetX() / cellSize + 1, imageSize.GetY() / cellSize + 1);
 	istd::TArray<i2d::CVector2d, 2> cornerArray;
 	// MESF ACF-version compatibility
 	cornerArray.SetSizes(gridSize);
 	cornerArray.SetAllElements(i2d::CVector2d(qInf(), qInf()));
 
+	const i2d::ICalibration2d* calibrationPtr = sourceAoi.GetCalibration();
+
 	istd::CIndex2d gridIndex;
 	for (gridIndex[1] = 0; gridIndex[1] < gridSize[1]; ++gridIndex[1]){
 		for (gridIndex[0] = 0; gridIndex[0] < gridSize[0]; ++gridIndex[0]){
 			i2d::CVector2d cornerDestPos(
-						sourceAoi.GetLeft() + sourceAoi.GetWidth() * gridIndex.GetX() * cellSize / imageSize.GetX(),
-						sourceAoi.GetTop() + sourceAoi.GetHeight() * gridIndex.GetY() * cellSize / imageSize.GetY());
+						sourceAoi.GetLeft() + sourceAoi.GetWidth() * gridIndex.GetX() * cellSize / outputImageSize.GetX(),
+						sourceAoi.GetTop() + sourceAoi.GetHeight() * gridIndex.GetY() * cellSize / outputImageSize.GetY());
 
 			i2d::CVector2d originalPos;
 
@@ -58,7 +65,8 @@ bool CImageCropDecalibrateProcessorComp::CropImage(
 	quint8* destBufferPtr = (quint8*)outputBitmap.GetLinePtr(0);
 	int destLineDiff = outputBitmap.GetLinesDifference();
 
-	i2d::CRectangle inputPosRectangle(I_BIG_EPSILON, I_BIG_EPSILON, imageSize.GetX() - 2 * I_BIG_EPSILON, imageSize.GetY() - 2 * I_BIG_EPSILON);
+	istd::CIndex2d inputImageSize = inputBitmap.GetImageSize();
+	i2d::CRectangle inputPosRectangle(I_BIG_EPSILON, I_BIG_EPSILON, inputImageSize.GetX() - 2 * I_BIG_EPSILON, inputImageSize.GetY() - 2 * I_BIG_EPSILON);
 
 	for (gridIndex[1] = 0; gridIndex[1] < gridSize[1] - 1; ++gridIndex[1]){
 		for (gridIndex[0] = 0; gridIndex[0] < gridSize[0] - 1; ++gridIndex[0]){
@@ -80,13 +88,15 @@ bool CImageCropDecalibrateProcessorComp::CropImage(
 						inputPosRectangle.Contains(posRightTop) &&
 						inputPosRectangle.Contains(posLeftBottom) &&
 						inputPosRectangle.Contains(posRightBottom)){
-				for (int y = 0; y < cellSize; ++y){
+				int maxY = qMin(cellSize, outputImageSize.GetY() - gridIndex[1] * cellSize);
+				for (int y = 0; y < maxY; ++y){
 					quint8* destLinePtr = destBufferPtr + gridIndex[0] * cellSize + (gridIndex[1] * cellSize + y) * destLineDiff;
 
 					i2d::CVector2d normPosLeft = (posLeftTop * (cellSize - y) + posLeftBottom * y) / (cellSize * cellSize);
 					i2d::CVector2d normPosRight = (posRightTop * (cellSize - y) + posRightBottom * y) / (cellSize * cellSize);
 
-					for (int x = 0; x < cellSize; ++x){
+					int maxX = qMin(cellSize, outputImageSize.GetX() - gridIndex[0] * cellSize);
+					for (int x = 0; x < maxX; ++x){
 						i2d::CVector2d sourcePos = normPosLeft * (cellSize - x) + normPosRight * x;
 
 						int sourceX = int(sourcePos.GetX());
@@ -97,13 +107,15 @@ bool CImageCropDecalibrateProcessorComp::CropImage(
 				}
 			}
 			else{
-				for (int y = 0; y < cellSize; ++y){
+				int maxY = qMin(cellSize, outputImageSize.GetY() - gridIndex[1] * cellSize);
+				for (int y = 0; y < maxY; ++y){
 					quint8* destLinePtr = destBufferPtr + gridIndex[0] * cellSize + (gridIndex[1] * cellSize + y) * destLineDiff;
 
 					i2d::CVector2d normPosLeft = (posLeftTop * (cellSize - y) + posLeftBottom * y) / (cellSize * cellSize);
 					i2d::CVector2d normPosRight = (posRightTop * (cellSize - y) + posRightBottom * y) / (cellSize * cellSize);
 
-					for (int x = 0; x < cellSize; ++x){
+					int maxX = qMin(cellSize, outputImageSize.GetX() - gridIndex[0] * cellSize);
+					for (int x = 0; x < maxX; ++x){
 						i2d::CVector2d sourcePos = normPosLeft * (cellSize - x) + normPosRight * x;
 
 						if (inputPosRectangle.Contains(sourcePos)){
@@ -125,6 +137,29 @@ bool CImageCropDecalibrateProcessorComp::CropImage(
 }
 
 
+bool CImageCropDecalibrateProcessorComp::CalcCalibration(
+			const i2d::CRectangle& sourceAoi,
+			icalib::CAffineCalibration2d& outputCalib)
+{
+	istd::CIndex2d outputImageSize;
+	if (!CalcOutputImageSize(sourceAoi, outputImageSize)){
+		return false;
+	}
+
+	double scaleX = outputImageSize.GetX() / sourceAoi.GetWidth();
+	double scaleY = outputImageSize.GetY() / sourceAoi.GetHeight();
+	i2d::CMatrix2d scaleMatrix(scaleX, 0, 0, scaleY);
+
+	i2d::CAffine2d transformation;
+	transformation.SetTranslation(-scaleMatrix.GetMultiplied(sourceAoi.GetLeftTop()));
+	transformation.SetDeformMatrix(scaleMatrix);
+
+	outputCalib.SetTransformation(transformation);
+
+	return true;
+}
+
+
 // reimplemented (iproc::IProcessor)
 
 int CImageCropDecalibrateProcessorComp::DoProcessing(
@@ -133,23 +168,62 @@ int CImageCropDecalibrateProcessorComp::DoProcessing(
 			istd::IChangeable* outputPtr,
 			ibase::IProgressManager* /*progressManagerPtr*/)
 {
-	const iimg::IBitmap* inputBitmapPtr = dynamic_cast<const iimg::IBitmap*>(inputPtr);
-	if (inputBitmapPtr == NULL){
-		return TS_INVALID;
-	}
-
-	iimg::IBitmap* outputBitmapPtr = dynamic_cast<iimg::IBitmap*>(outputPtr);
-	if (outputBitmapPtr == NULL){
-		return TS_INVALID;
-	}
-
 	iprm::TParamsPtr<i2d::CRectangle> aoiParamPtr(paramsPtr, m_aoiParamIdAttrPtr, m_defaultAoiCompPtr, true);
 	if (!aoiParamPtr.IsValid()){
 		SendErrorMessage(MI_BAD_PARAMS, QObject::tr("No AOI rectangle found"));
 		return TS_INVALID;
 	}
 
-	return CropImage(*inputBitmapPtr, *aoiParamPtr, *outputBitmapPtr) ? TS_OK : TS_INVALID;
+	iimg::IBitmap* outputBitmapPtr = dynamic_cast<iimg::IBitmap*>(outputPtr);
+	if (outputBitmapPtr == NULL){
+		icalib::CAffineCalibration2d* outputCalibPtr = dynamic_cast<icalib::CAffineCalibration2d*>(outputPtr);
+		if (outputCalibPtr != NULL){
+			return CalcCalibration(*aoiParamPtr, *outputCalibPtr)? TS_OK: TS_INVALID;
+		}
+
+		return TS_INVALID;
+	}
+
+	const iimg::IBitmap* inputBitmapPtr = dynamic_cast<const iimg::IBitmap*>(inputPtr);
+	if (inputBitmapPtr == NULL){
+		return TS_INVALID;
+	}
+
+	return CropImage(*aoiParamPtr, *m_cellSizeAttrPtr, *inputBitmapPtr, *outputBitmapPtr)? TS_OK: TS_INVALID;
+}
+
+
+// protected methods
+
+bool CImageCropDecalibrateProcessorComp::CalcOutputImageSize(
+			const i2d::CRectangle& sourceAoi,
+			istd::CIndex2d& result)
+{
+	const i2d::ICalibration2d* calibrationPtr = sourceAoi.GetCalibration();
+
+	double pixelSize = 1;
+
+	i2d::CVector2d inputLeftTopPos = sourceAoi.GetLeftTop();
+	i2d::CVector2d inputRightTopPos = sourceAoi.GetRightTop();
+	i2d::CVector2d inputLeftBottomPos = sourceAoi.GetLeftBottom();
+	i2d::CVector2d inputRightBottomPos = sourceAoi.GetRightBottom();
+	if (calibrationPtr != NULL){
+		double leftDist = 0;
+		double rightDist = 0;
+		double topDist = 0;
+		double bottomDist = 0;
+		if (
+					calibrationPtr->GetDistance(sourceAoi.GetLeftTop(), sourceAoi.GetLeftBottom(), leftDist) &&
+					calibrationPtr->GetDistance(sourceAoi.GetRightTop(), sourceAoi.GetRightBottom(), rightDist) &&
+					calibrationPtr->GetDistance(sourceAoi.GetLeftTop(), sourceAoi.GetRightTop(), topDist) &&
+					calibrationPtr->GetDistance(sourceAoi.GetLeftBottom(), sourceAoi.GetRightBottom(), bottomDist)){
+			pixelSize = 0.5 * (leftDist + rightDist + topDist + bottomDist) / (sourceAoi.GetWidth() + sourceAoi.GetHeight());
+		}
+	}
+
+	result = istd::CIndex2d(qCeil(sourceAoi.GetWidth() * pixelSize),  qCeil(sourceAoi.GetHeight() * pixelSize));
+
+	return true;
 }
 
 
--- Include/iipr/CImageCropDecalibrateProcessorComp.h
+++ Include/iipr/CImageCropDecalibrateProcessorComp.h
@@ -10,6 +10,7 @@
 #include <i2d/CRectangle.h>
 
 // ACF-Solutions includes
+#include <icalib/CAffineCalibration2d.h>
 #include <iproc/TSyncProcessorCompBase.h>
 
 
@@ -29,12 +30,18 @@ public:
 		I_REGISTER_INTERFACE(iproc::IProcessor);
 		I_ASSIGN(m_aoiParamIdAttrPtr, "AoiParamId", "ID of area of interest in parameter set (i2d::CRectangle)", false, "AoiParams");
 		I_ASSIGN(m_defaultAoiCompPtr, "DefaultAoi", "Area of interest used if not specified in parameters", false, "DefaultAoi");
+		I_ASSIGN(m_cellSizeAttrPtr, "CellSize", "Size of single cell (inside of cell linear interpolation will be used)", true, 8);
 	I_END_COMPONENT;
 
 	static bool CropImage(
-				const iimg::IBitmap& inputBitmap,
 				const i2d::CRectangle& sourceAoi,
+				int cellSize,
+				const iimg::IBitmap& inputBitmap,
 				iimg::IBitmap& outputBitmap);
+
+	static bool CalcCalibration(
+				const i2d::CRectangle& sourceAoi,
+				icalib::CAffineCalibration2d& outputCalib);
 
 	// reimplemented (iproc::IProcessor)
 	virtual int DoProcessing(
@@ -43,9 +50,15 @@ public:
 				istd::IChangeable* outputPtr,
 				ibase::IProgressManager* progressManagerPtr = NULL);
 
+protected:
+	static bool CalcOutputImageSize(
+				const i2d::CRectangle& sourceAoi,
+				istd::CIndex2d& result);
+
 private:
 	I_ATTR(QByteArray, m_aoiParamIdAttrPtr);
 	I_REF(i2d::CRectangle, m_defaultAoiCompPtr);
+	I_ATTR(int, m_cellSizeAttrPtr);
 };
 
 
