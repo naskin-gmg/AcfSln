eb98f1ea10ccb1386982d9df8bbb56e0db273ce2
wg <wg@3363cc13-2b78-0410-9281-ee1caeb4462d>
2017-05-24 15:26:29 +0000

    Image crop processor extended to support image cropping as static function.
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfSln@1845 3363cc13-2b78-0410-9281-ee1caeb4462d

--- Include/iipr/CImageCropProcessorComp.cpp
+++ Include/iipr/CImageCropProcessorComp.cpp
@@ -15,6 +15,95 @@ namespace iipr
 {
 
 
+bool CImageCropProcessorComp::DoImageCrop(
+			const iimg::IBitmap& inputBitmap,
+			const i2d::IObject2d& aoi,
+			iimg::IBitmap& outputBitmap,
+			istd::ILogger* loggerPtr)
+{
+	istd::CIndex2d inputBitmapSize = inputBitmap.GetImageSize();
+
+	iimg::CScanlineMask bitmapRegion;
+	i2d::CRect clipArea(inputBitmapSize);
+	if (!bitmapRegion.CreateFromGeometry(aoi, &clipArea)){
+		if (loggerPtr != NULL){
+			loggerPtr->SendLogMessage(istd::IInformationProvider::IC_ERROR, 0, "Cannot create the region", "Image Crop");
+		}
+
+		return false;
+	}
+
+	i2d::CRect regionRect = bitmapRegion.GetBoundingBox();
+
+	int regionLeft = qMax(regionRect.GetLeft(), 0);
+	int regionRight = qMin(regionRect.GetRight(), inputBitmapSize.GetX());
+	int regionTop = qMax(regionRect.GetTop(), 0);
+	int regionBottom = qMin(regionRect.GetBottom(), inputBitmapSize.GetY());
+
+	if ((regionRight <= regionLeft) || (regionBottom <= regionTop)){
+		outputBitmap.ResetImage();
+
+		return true;
+	}
+
+	ibase::CSize outputBitmapSize(
+				int(regionRight - regionLeft),
+				int(regionBottom - regionTop));
+	if (!outputBitmap.CreateBitmap(inputBitmap.GetPixelFormat(), outputBitmapSize)){
+		if (loggerPtr != NULL){
+			loggerPtr->SendLogMessage(istd::IInformationProvider::IC_ERROR, 0, "Cannot create bitmap", "Image Crop");
+		}
+
+		return false;
+	}
+
+	// CreateBitmap does NOT clear the image (performance reasons?)
+	outputBitmap.ClearImage();
+
+	int pixelBits = outputBitmap.GetPixelBitsCount();
+	int pixelBytes = pixelBits / 8;
+
+	if (pixelBytes * 8 != pixelBits){
+		return false;
+	}
+	
+	for (int y = regionTop; y < regionBottom; y++){
+		const quint8* inputLinePtr = (quint8*)inputBitmap.GetLinePtr(y);
+
+		istd::CIntRanges::RangeList rangesList;
+		const istd::CIntRanges* rangesPtr = bitmapRegion.GetPixelRanges(y);
+		if (rangesPtr == NULL){
+			continue;
+		}
+
+		rangesPtr->GetAsList(clipArea.GetHorizontalRange(), rangesList);
+
+		quint8* outputImageLinePtr = (quint8*)outputBitmap.GetLinePtr(y - regionTop);
+
+		for (		istd::CIntRanges::RangeList::ConstIterator iter = rangesList.begin();
+					iter != rangesList.end();
+					++iter){
+			const istd::CIntRange& pixelRange = *iter;
+
+			int rangeStart = qMax(pixelRange.GetMinValue(), 0);
+			Q_ASSERT(rangeStart >= regionLeft);
+			int rangeEnd = qMin(pixelRange.GetMaxValue(), inputBitmap.GetImageSize().GetX());
+			Q_ASSERT(rangeEnd <= regionRight);
+
+			int bytesToCopy = (rangeEnd - rangeStart) * pixelBytes;
+
+			if (bytesToCopy > 0){
+				std::memcpy(	outputImageLinePtr + (rangeStart - regionLeft) * pixelBytes,
+								inputLinePtr + rangeStart * pixelBytes,
+								bytesToCopy);
+			}
+		}
+	}
+
+	return true;
+}
+
+
 // protected methods
 
 // reimplemented (CImageRegionProcessorCompBase)
@@ -44,80 +133,7 @@ bool CImageCropProcessorComp::ProcessImageRegion(
 		return false;
 	}
 
-	istd::CIndex2d inputBitmapSize = inputBitmap.GetImageSize();
-
-	iimg::CScanlineMask bitmapRegion;
-	i2d::CRect clipArea(inputBitmapSize);
-	if (!bitmapRegion.CreateFromGeometry(*aoiPtr, &clipArea)){
-		SendWarningMessage(0, "Cannot create the region");
-
-		return false;
-	}
-
-	i2d::CRect regionRect = bitmapRegion.GetBoundingBox();
-
-	int regionLeft = qMax(regionRect.GetLeft(), 0);
-	int regionRight = qMin(regionRect.GetRight(), inputBitmapSize.GetX());
-	int regionTop = qMax(regionRect.GetTop(), 0);
-	int regionBottom = qMin(regionRect.GetBottom(), inputBitmapSize.GetY());
-
-	if ((regionRight <= regionLeft) || (regionBottom <= regionTop)){
-		outputBitmapPtr->ResetImage();
-
-		return true;
-	}
-
-	ibase::CSize outputBitmapSize(
-				int(regionRight - regionLeft),
-				int(regionBottom - regionTop));
-	if (!outputBitmapPtr->CreateBitmap(inputBitmap.GetPixelFormat(), outputBitmapSize)){
-		return false;
-	}
-
-	// CreateBitmap does NOT clear the image (performance reasons?)
-	outputBitmapPtr->ClearImage();
-
-	int pixelBits = outputBitmapPtr->GetPixelBitsCount();
-	int pixelBytes = pixelBits / 8;
-
-	if (pixelBytes * 8 != pixelBits){
-		return false;
-	}
-	
-	for (int y = regionTop; y < regionBottom; y++){
-		const quint8* inputLinePtr = (quint8*)inputBitmap.GetLinePtr(y);
-
-		istd::CIntRanges::RangeList rangesList;
-		const istd::CIntRanges* rangesPtr = bitmapRegion.GetPixelRanges(y);
-		if (rangesPtr == NULL){
-			continue;
-		}
-
-		rangesPtr->GetAsList(clipArea.GetHorizontalRange(), rangesList);
-
-		quint8* outputImageLinePtr = (quint8*)outputBitmapPtr->GetLinePtr(y - regionTop);
-
-		for (		istd::CIntRanges::RangeList::ConstIterator iter = rangesList.begin();
-					iter != rangesList.end();
-					++iter){
-			const istd::CIntRange& pixelRange = *iter;
-
-			int rangeStart = qMax(pixelRange.GetMinValue(), 0);
-			Q_ASSERT(rangeStart >= regionLeft);
-			int rangeEnd = qMin(pixelRange.GetMaxValue(), inputBitmap.GetImageSize().GetX());
-			Q_ASSERT(rangeEnd <= regionRight);
-
-			int bytesToCopy = (rangeEnd - rangeStart) * pixelBytes;
-
-			if (bytesToCopy > 0){
-				std::memcpy(	outputImageLinePtr + (rangeStart - regionLeft) * pixelBytes,
-								inputLinePtr + rangeStart * pixelBytes,
-								bytesToCopy);
-			}
-		}
-	}
-
-	return true;
+	return DoImageCrop(inputBitmap, *aoiPtr, *outputBitmapPtr, const_cast<CImageCropProcessorComp*>(this));
 }
 
 
--- Include/iipr/CImageCropProcessorComp.h
+++ Include/iipr/CImageCropProcessorComp.h
@@ -3,6 +3,9 @@
 
 
 // ACF-Solutions includes
+#include <istd/ILogger.h>
+
+// ACF-Solutions includes
 #include <imeas/IDiscreteDataSequence.h>
 #include <iipr/CImageRegionProcessorCompBase.h>
 
@@ -22,6 +25,13 @@ public:
 	I_BEGIN_COMPONENT(CImageCropProcessorComp);
 	I_END_COMPONENT;
 
+	static bool DoImageCrop(
+				const iimg::IBitmap& inputBitmap,
+				const i2d::IObject2d& aoi,
+				iimg::IBitmap& outputBitmap,
+				istd::ILogger* loggerPtr = NULL);
+
+protected:
 	// reimplemented (CImageRegionProcessorCompBase)
 	virtual bool ProcessImageRegion(
 				const iimg::IBitmap& inputBitmap,
