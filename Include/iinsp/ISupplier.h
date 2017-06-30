a452601bee34b581c58d3d8dc9752e14e67815e3
wg <wg@3363cc13-2b78-0410-9281-ee1caeb4462d>
2017-06-30 14:59:57 +0000

    Comments updated.
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfSln@1867 3363cc13-2b78-0410-9281-ee1caeb4462d

--- Include/iinsp/ISupplier.h
+++ Include/iinsp/ISupplier.h
@@ -25,11 +25,11 @@ public:
 	enum WorkStatus
 	{
 		/**
-			Supplier is invalidated and will produce new result on demand.
+			Supplier is invalidated, no result is produced.
 		*/
 		WS_INVALID,
 		/**
-			New work is initialized.
+			New work is initialized and will produce new result on demand.
 		*/
 		WS_INIT,
 		/**
@@ -44,9 +44,8 @@ public:
 			Work was canceled.
 		*/
 		WS_CANCELED,
-
 		/**
-			Work cannot be done.
+			Work couldn't be done.
 		*/
 		WS_FAILED
 	};
