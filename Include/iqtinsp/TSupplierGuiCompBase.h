614da7344a9d36e71be27d17b5681a869ec4352c
kl <kl@3363cc13-2b78-0410-9281-ee1caeb4462d>
2019-03-22 09:12:12 +0000

    Mac build fixed
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfSln@2050 3363cc13-2b78-0410-9281-ee1caeb4462d

--- Include/iqtinsp/TSupplierGuiCompBase.h
+++ Include/iqtinsp/TSupplierGuiCompBase.h
@@ -36,6 +36,9 @@ public:
 	virtual void RemoveItemsFromScene(iqt2d::IViewProvider* providerPtr);
 
 protected:
+	using BaseClass::GetObservedObject;
+	typedef typename BaseClass::Shapes Shapes;
+
 	/**
 		Extract calibration provider from the observed object.
 	*/
