f11b2f44e7db944d845334c12a493e22111d26f1
kl <kl@3363cc13-2b78-0410-9281-ee1caeb4462d>
2019-05-10 15:07:15 +0000

    Message implementation (with persistence support) for transport of 2D-objects added.
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfSln@2064 3363cc13-2b78-0410-9281-ee1caeb4462d

---
+++ Include/iipr/CGeometryMessage.cpp
@@ -0,0 +1,35 @@
+#include <iipr/CGeometryMessage.h>
+
+
+namespace iipr
+{
+
+
+// public methods
+
+CGeometryMessage::CGeometryMessage()
+	:BaseClass(&s_factory)
+{
+}
+
+
+CGeometryMessage::CGeometryMessage(
+			istd::IInformationProvider::InformationCategory category,
+			int id,
+			const QString& text,
+			const QString& source,
+			int flags,
+			const QDateTime* timeStampPtr)
+	:BaseClass(category, id, text, source, flags, timeStampPtr, &s_factory)
+{
+}
+
+
+// private static members
+
+i2d::CObject2dFactory CGeometryMessage::s_factory;
+
+
+} // namespace iipr
+
+
---
+++ Include/iipr/CGeometryMessage.h
@@ -0,0 +1,40 @@
+#ifndef iipr_CGeometryMessage_included
+#define iipr_CGeometryMessage_included
+
+
+// ACF includes
+#include <ilog/CExtMessage.h>
+#include <i2d/CObject2dFactory.h>
+
+
+namespace iipr
+{
+
+
+/**
+	Message implementation for transport of 2D-objects.
+*/
+class CGeometryMessage: public ilog::CExtMessage
+{
+public:
+	typedef ilog::CExtMessage BaseClass;
+
+	CGeometryMessage();
+	CGeometryMessage(istd::IInformationProvider::InformationCategory category,
+				int id,
+				const QString& text,
+				const QString& source,
+				int flags = 0,
+				const QDateTime* timeStampPtr = NULL);
+
+private:
+	static i2d::CObject2dFactory s_factory;
+};
+
+
+} // namespace iipr
+
+
+#endif // !iipr_CGeometryMessage_included
+
+
