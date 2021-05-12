f6ccd9a1d197fb4c31df9d57307e32ebfab4c93d
kl <kl@3363cc13-2b78-0410-9281-ee1caeb4462d>
2021-05-12 19:52:18 +0000

    Cmake preconf added
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfSln@2145 3363cc13-2b78-0410-9281-ee1caeb4462d

---
+++ Config/CMake/AcfSlnEnv.cmake
@@ -0,0 +1,22 @@
+#Acf
+
+if(NOT DEFINED ACFCONFIGDIR)
+	set(ACFCONFIGDIR "$ENV{ACFCONFIGDIR}")
+endif()
+
+include(${ACFCONFIGDIR}/CMake/AcfEnv.cmake)
+
+
+#Sln
+if(NOT DEFINED ACFSLNDIR)
+	set(ACFSLNDIR "$ENV{ACFSLNDIR}")
+endif()
+if(NOT DEFINED CONFIGSLN_DIR)
+	set(CONFIGSLN_DIR "$ENV{ACFSLNDIR}/Config")
+endif()
+
+
+include_directories("${ACFSLNDIR}/AuxInclude/Qt${QT_DEFAULT_MAJOR_VERSION}_${TARGETNAME}")
+include_directories("${ACFSLNDIR}/Include")
+include_directories("${ACFSLNDIR}/Impl")
+
