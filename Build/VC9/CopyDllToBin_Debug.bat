0b2540d97b518da4a1c898d0e645bd3f935adb4e
kl <kl@3363cc13-2b78-0410-9281-ee1caeb4462d>
2022-04-17 06:46:34 +0000

    Removed deprecated stuff
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfSln@2167 3363cc13-2b78-0410-9281-ee1caeb4462d

--- Build/VC9/CopyDllToBin_Debug.bat
+++
@@ -1,6 +0,0 @@
-copy /Y %QTDIR%\bin.debug\*.dll ..\..\Bin\DebugVC9
-xcopy /Y %QTDIR%\plugins\imageformats\*.* ..\..\Bin\DebugVC9\imageformats\*.*
-xcopy /Y %QTDIR%\plugins\iconengines\*.* ..\..\Bin\DebugVC9\iconengines\*.*
-
-copy /Y %ACFDIR%\Bin\ReleaseVC8\Acf.exe ..\..\Bin\DebugVC9
-
--- Build/VC9/CopyDllToBin_Release.bat
+++
@@ -1,6 +0,0 @@
-copy /Y %QTDIR%\bin\*.dll ..\..\Bin\ReleaseVC9
-xcopy /Y %QTDIR%\plugins\imageformats\*.* ..\..\Bin\ReleaseVC9\imageformats\*.*
-xcopy /Y %QTDIR%\plugins\iconengines\*.* ..\..\Bin\ReleaseVC9\iconengines\*.*
-
-copy /Y %ACFDIR%\Bin\ReleaseVC8\Acf.exe ..\..\Bin\ReleaseVC9
-
