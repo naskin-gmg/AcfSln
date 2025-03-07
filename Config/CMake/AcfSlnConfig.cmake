b52eb0cf8ef1dff06cd2fd31e19a593df897495c
Roman.Kucherenko <Roman.Kucherenko@3363cc13-2b78-0410-9281-ee1caeb4462d>
2025-03-07 04:36:41 +0000

    Cmake formatting corrected
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfSln@2235 3363cc13-2b78-0410-9281-ee1caeb4462d

--- Config/CMake/AcfSlnConfig.cmake
+++ Config/CMake/AcfSlnConfig.cmake
@@ -1,4 +1,4 @@
 function(acfsln_get_root_dir identifier_to_use)
-    set(${identifier_to_use} "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../../.." PARENT_SCOPE)
+	set(${identifier_to_use} "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../../.." PARENT_SCOPE)
 endfunction()
 
