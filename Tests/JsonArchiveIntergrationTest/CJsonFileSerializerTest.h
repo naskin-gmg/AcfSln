bd8d4a068a3973c2ad8f91d9b106b8b0a7b17b52
kl <kl@3363cc13-2b78-0410-9281-ee1caeb4462d>
2025-05-25 13:55:42 +0000

    Cosetmics changes
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfSln@2245 3363cc13-2b78-0410-9281-ee1caeb4462d

--- Tests/JsonArchiveIntergrationTest/CJsonFileSerializerTest.h
+++ Tests/JsonArchiveIntergrationTest/CJsonFileSerializerTest.h
@@ -1,25 +1,3 @@
-/********************************************************************************
-**
-**	Copyright (C) 2007-2017 Witold Gantzke & Kirill Lepskiy
-**
-**	This file is part of the ACF Toolkit.
-**
-**	This file may be used under the terms of the GNU Lesser
-**	General Public License version 2.1 as published by the Free Software
-**	Foundation and appearing in the file LicenseLGPL.txt included in the
-**	packaging of this file.  Please review the following information to
-**	ensure the GNU Lesser General Public License version 2.1 requirements
-**	will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
-**
-**	If you are unsure which license is appropriate for your use, please
-**	contact us at info@imagingtools.de.
-**
-** 	See http://www.ilena.org or write info@imagingtools.de for further
-** 	information about the ACF.
-**
-********************************************************************************/
-
-
 #pragma once
 
 
